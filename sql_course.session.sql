f6895c42af7a0c174e2cf5d620e2e1814925d48f
Edwinraj <prgdurai@gmail.com>
2025-01-19 16:16:43 -0700

    New

--- sql_course.session.sql
+++ sql_course.session.sql
@@ -56,3 +56,84 @@ WHERE
 salary_year_avg > 70000 AND
 job_title_short = 'Data Analyst'
 ORDER BY salary_year_avg DESC
+
+
+SELECT job_id,
+    COUNT(*) AS job_count
+FROM january_job
+GROUP BY job_id
+HAVING COUNT(*) > 1
+
+INSERT INTO january_job1(SELECT * FROM job_postings_fact
+WHERE job_id = 105)
+
+SELECT * FROM january_job
+WHERE job_id = 105
+
+DELETE FROM january_job AS a
+USING january_job AS b
+WHERE a.job_id = b.job_id
+
+SELECT ROW_NUMBER() OVER (ORDER BY job_id) AS row_no,
+job_id FROM january_job AS q1 (DELETE FROM january_job
+WHERE row_no NOT IN (
+SELECT MIN(row_no)
+FROM q1))
+
+
+SELECT ROW_NUMBER() OVER (ORDER BY job_id) AS row_no,
+job_id FROM january_job AS q1
+DELETE FROM january_job
+WHERE row_no NOT IN (
+SELECT MIN(row_no)
+FROM q1)
+
+
+SELECT ROW_NUMBER() OVER (ORDER BY job_id) AS row_no,
+job_id FROM january_job
+
+INSERT INTO january_job (row_no) 
+SELECT ROW_NUMBER() OVER (ORDER BY job_id) 
+AS row_no FROM january_job
+
+DROP TABLE january_job1
+
+SELECT * FROM
+january_job1
+
+CREATE TABLE january_job1 AS
+--INSERT INTO january_job1
+--SELECT ROW_NUMBER() OVER (ORDER BY job_id), 
+SELECT * FROM january_job
+
+INSERT INTO january_job1 VALUES(
+ROW_NUMBER() OVER (ORDER BY job_id), 
+* FROM january_job)
+
+ALTER TABLE january_job 
+ADD ROW_NUMBER() OVER (ORDER BY job_id) AS row_no INT;
+
+DELETE FROM january_job1
+WHERE row_number NOT IN (
+SELECT MAX(row_number) FROM january_job1)
+
+DELETE FROM january_job1
+WHERE row_number NOT IN (
+ SELECT MAX(row_number)
+ FROM january_job1
+ GROUP BY job_id)
+
+SELECT job_id,
+COUNT(*) AS row_count
+FROM january_job1
+GROUP BY job_id
+HAVING COUNT(*) > 1
+
+select *, CTID
+FROM january_job1
+WHERE job_id = 105
+
+DELETE FROM january_job1
+WHERE CTID NOT IN(SELECT MIN(CTID) FROM january_job1
+GROUP BY job_id, company_id
+HAVING COUNT(*)> 1)
